9514f9b61bfbbf97cce57e15792e4909b45e6cf2
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-10 16:25:11 +0100

    added calculation for convergence rate

--- mkhmhm.py
+++ mkhmhm.py
@@ -23,7 +23,7 @@ def leapfrog( steps ):
        global space.
     """
 
-    delta = 2.0*cycles*np.pi/omega/steps
+    delta = (2.0*cycles*np.pi/omega)/steps
     x     = np.empty( steps+1 )
     v     = np.empty( steps+1 )
     t     = np.empty( steps+1 )
@@ -42,15 +42,17 @@ def leapfrog( steps ):
         x[i+1] = x[i] + delta*v[i+1]
     return t, x, v
 
-def l2_error_norm( t , x ):
+def l2_error_norm(t, x):
     """Calculate the L2 relative error norm."""
-    steps  = len( x ) - 1
+    steps = len( x ) - 1
     l2_err = 0.0
     l2     = 0.0
+    l2_ref = 0.0
     for i in range(len(x)):
         x_exact = x0*np.cos( omega*t[i] )
-        l2_err += (x[i] - x_exact)**2.0
-        l2     += x[i]**2.0
+        l2_err += ((x[i] - x_exact)**2.0)
+        l2     += (x[i]**2.0)
+        l2_ref += (x_exact**2.0)
     return (l2_err/l2)**0.5
 
 # The backend choice here may be platform dependent. You may need to
@@ -90,18 +92,18 @@ plt.tight_layout()
 # Switch to a new plotting window, and plot the L2 error norm,
 # with guidelines for first, second, and third order accuracy.
 plt.figure()
-plt.loglog( delta , l2_error , 'o', label="L2 Error numerical results" )
+plt.loglog( delta , l2_error, 'o', label="L2 Error numerical results" )
 plt.loglog( delta , l2_error[0]*(delta/delta[0])**1.0, label='1st order accuracy' )
 plt.loglog( delta , l2_error[0]*(delta/delta[0])**2.0, label='2nd order accuracy' )
 plt.loglog( delta , l2_error[0]*(delta/delta[0])**3.0, label='3rd order accuracy' )
 
 #calculating convergence rate (slope of log.log)
-fine_region = slice(-5, None) #using smallest dt values for better acuracy
+fine_region = slice(-14, None) #using smallest dt values for better acuracy
 fit = np.polyfit(np.log(delta[fine_region]), np.log(l2_error[fine_region]), 1)
 slope = fit[0]
 
 fit_line = np.exp(fit[1]) *delta**slope
-plt.loglog(delta, fit_line, '--', label=f'Fitted slope = {slope:2f}')
+plt.loglog(delta, fit_line, '--', label=f'Fitted slope = {slope:3f}')
 
 
 plt.xlabel('Δt x ω')
